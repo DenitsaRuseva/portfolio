f1d769afd86b42291917c4ca491dcd2b85c2f127
DenicaRuseva <drr_87@abv.bg>
2020-06-05 19:46:22 +0100

    nav icons

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem';
-import { CSSTransitionGroup } from 'react-transition-group';
+import {faHome, faUserSecret, faCameraRetro, faShoppingCart, faLocationArrow} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 export default function(props){
     return(
         <nav className='navigation-items'>
-            {/* <CSSTransitionGroup> */}
-            <NavigationItem link='/'>HOME</NavigationItem>
-            <NavigationItem link='/link1' >LINK1</NavigationItem>
-            <NavigationItem link='/link2' >LINK2</NavigationItem>
-            <NavigationItem link='/about'>ABOUT</NavigationItem>
-            {/* </CSSTransitionGroup> */}
+            <NavigationItem link='/'><span><span>HOME</span><FontAwesomeIcon icon={faHome} className='nav-svg'>
+                </FontAwesomeIcon></span>
+                
+            </NavigationItem>
+            <NavigationItem link='/gallery'><span><span>GALLERY</span><FontAwesomeIcon icon={faCameraRetro} className='nav-svg'></FontAwesomeIcon></span>
+                </NavigationItem>
+            <NavigationItem link='/shop' ><span><span>SHOP</span><FontAwesomeIcon icon={faShoppingCart} className='nav-svg'></FontAwesomeIcon></span>
+                </NavigationItem>
+            <NavigationItem link='/about'><span><span>ABOUT</span><FontAwesomeIcon icon={faUserSecret} className='nav-svg'></FontAwesomeIcon></span>
+                </NavigationItem>
+                <NavigationItem link='/contacts' ><span><span>CONTACTS</span><FontAwesomeIcon icon={faLocationArrow} className='nav-svg'></FontAwesomeIcon></span>
+                </NavigationItem>
         </nav>
     );
 };
