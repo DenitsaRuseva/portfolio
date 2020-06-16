a6a614f30a1923c70f5c6ade661942240a80717a
DenicaRuseva <drr_87@abv.bg>
2020-06-16 19:29:50 +0100

    text container, btns container animations

--- src/components/Home/Portfolio/Portfolio.js
+++ src/components/Home/Portfolio/Portfolio.js
@@ -1,33 +1,60 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import Button from 'components/UI/Button/Button';
 import './Portfolio.css';
 
 
 export default function Portfolio(){
 
+    const showHeaderRef = useRef(null);
+    const [showHeader, setShowHeader] = useState(false);
     const [actveBtnIndex, setActiveBtnIndex] = useState(0);
+
+   
+    function listener(){
+        if(window.scrollY >= window.pageXOffset && showHeaderRef.current === null){
+            showHeaderRef.current = true;
+            setShowHeader(true);
+            console.log('jdkfjlsdk')
+        };
+        
+    };
+    function removeListener(){
+        window.removeEventListener('scroll', listener);
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', listener);
+        return removeListener;
+    }, []);
 
     const toggleActiveButton = (index) => {
         setActiveBtnIndex(index);
     };
 
+    const headerClasses = ['portfolio-text-container'];
+    const btnContClasses = ['portfolio-buttons-container'];
+    if(showHeader){
+        headerClasses.push('show-text');
+        btnContClasses.push('show-btns');
+    };
 
     return(
         <div className='portfolio'>
-            <div className='portfolio-text-container'>
-                <h1>PORTFOLIO</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</p>
-                <div className='portfolio-svg-container'>
-                    <div className='line left'></div>
-                    <div className='line right'></div>
+            <div className={headerClasses.join(' ')}>
+                <div className='portfolio-text-wrapp'>                
+                    <h1>PORTFOLIO</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</p>
+                    <div className='portfolio-svg-container'>
+                        <div className='line left'></div>
+                        <div className='line right'></div>
+                    </div>
                 </div>
+
             </div>
-            <div className='portfolio-buttons-container'>
-                <Button class={actveBtnIndex === 0 ? 'btn active' : 'btn'} clicked={() => toggleActiveButton(0)}>ALL</Button>
-                <Button class={actveBtnIndex === 1 ? 'btn active' : 'btn'} clicked={() => toggleActiveButton(1)}>NATURE</Button>
-                <Button class={actveBtnIndex === 2 ? 'btn active' : 'btn'} clicked={() => toggleActiveButton(2)}>PEOPLE</Button>
-                <Button class={actveBtnIndex === 3 ? 'btn active' : 'btn'} clicked={() => toggleActiveButton(3)}>STREET</Button>
-                <Button class={actveBtnIndex === 4 ? 'btn active' : 'btn'} clicked={() => toggleActiveButton(4)}>STILL LIFE</Button>
+            <div className={btnContClasses.join(' ')}>
+                    <Button class={actveBtnIndex === 0 ? 'btn active' : 'btn'} clicked={() => toggleActiveButton(0)}>ALL</Button>
+                    <Button class={actveBtnIndex === 1 ? 'btn active' : 'btn'} clicked={() => toggleActiveButton(1)}>STILL LIFE</Button>
+                    <Button class={actveBtnIndex === 2 ? 'btn active' : 'btn'} clicked={() => toggleActiveButton(2)}>LANDSCAPE</Button>
             </div>
             <div className='portfolio-gallery'></div>
         </div>
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './Layout.css';
 import Toolbar from 'components/Navigation/Toolbar/Toolbar';
 import Logo from 'components/Logo/Logo';
-import DrawerToggle from 'components/Navigation/Toolbar/DrawerToggle/DrawerToggle';
 
 
 export default function Layout(props) {
+
+    
+
     return (
     <div className='layout'>
         <Logo/>
