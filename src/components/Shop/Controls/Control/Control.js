7ec2c4a718d0cb7bd2e57cddbd97d24e71c3bcef
DenicaRuseva <drr_87@abv.bg>
2020-09-03 21:39:17 +0100

    controls, control

---
+++ src/components/Shop/Controls/Control/Control.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './Control.css';
+import {faPaintBrush, faFlask, faWindowClose, faChevronDown} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export default function control(props){
+
+
+    const selectContainerClasses = props.showOptions ? 'select-container show' : 'select-container';
+
+    const options = [];
+    for(let i=1; i<props.options.length; i++){
+        options.push(
+        <div className='option control-box' onClick={() => props.clickedOption(props.name,props.options[i].value)}>
+            <div className='select-svg-container'>
+                <FontAwesomeIcon icon={faPaintBrush}/>
+            </div>
+        <span>{props.options[i].displayValue}</span>
+    </div>);
+    };
+    options.push(
+        <div className='option control-box' onClick={() => props.clickedOption(props.name,props.options[0].value)}>
+                <div className='select-svg-container'>
+                        <FontAwesomeIcon icon={faWindowClose}/>
+                </div>
+                <span>Remove filter</span>
+        </div>
+    );
+
+    return(
+        <div className='shop-controls-container'>
+                <div className={selectContainerClasses}>
+                    <div className='select-label control-box'>{props.name.toString().toUpperCase()}:</div>
+                    <div className='select-options-wrapp'>
+                        <div className='select-value-container control-box' onClick={props.clicked}>
+                            <span>{props.options.find(el => el.value === props.value).displayValue}</span>
+                            <div className='select-svg-container'>
+                                <FontAwesomeIcon icon={faChevronDown}/>
+                            </div>
+                        </div>
+                        <div className='select-options-container'>
+                            {options}
+                        </div>
+                    </div>
+                </div>
+            </div>
+    );
+};
--- src/components/Shop/Controls/Controls.js
+++ src/components/Shop/Controls/Controls.js
@@ -2,60 +2,122 @@ import React, {useState} from 'react';
 import './Controls.css';
 import {faPaintBrush, faFlask, faWindowClose, faChevronDown} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Control from './Control/Control';
+import { updateObject } from 'utility';
 
-export default function Controls(props){
-    const [category, setCategory] = useState('selectCat');
-    const [showCategory, setShowCategory] = useState(false);
+export default function Controls(){
 
+    const [form, setForm] = useState({
+        collections: {
+            options: [
+            {value:'select', displayValue: 'Choose collection'},
+            {value:'nature', displayValue: 'Nature Morte'},
+            {value: 'bw', displayValue: 'Black and White'}],
+            value: 'select',
+            showOptions: false
+        },
+        colours: {
+            options: [
+                {value: 'select', displayValue: 'Choose colour'},
+                {value: 'blue', displayValue: 'Blue'},
+                {value: 'green', displayValue: 'Green'},
+                {value: 'purplle', displayValue: 'Purplle'}
+            ],
+            value: 'select',
+            showOptions: false
+        },
+        orientation: {
+            options: [
+                {value: 'select', displayValue: 'Choose orientation'},
+                {value: 'portret', displayValue: 'Portret'},
+                {value: 'landskape', displayValue: 'Landscape'}
+            ],
+            value: 'select',
+            showOptions: false
+        }
+    });
 
-    const categoriesOptions = [{value:'selectCat', displayValue: 'Choose your type'},
-                {value:'oil', displayValue: 'Oil paintings'},
-                {value: 'acrilic', displayValue: 'Acrilic paintings'}];
-
-    const changeCategory = (newCategoryValue) => {
-        setCategory(newCategoryValue);
+    const toggleShowOptionsHandler = (formProperty) => {
+        const updatedField = updateObject(form[formProperty], {showOptions: !form[formProperty].showOptions});
+        const updatedForm = updateObject(form, {[formProperty]: updatedField});
+        setForm(updatedForm);
+    };
+    const chooseOptionHandler = (formProperty, value) => {
+        const updatedField = updateObject(form[formProperty], {value: value, showOptions: false});
+        const updatedForm = updateObject(form, {[formProperty]: updatedField});
+        setForm(updatedForm);
     };
 
-    const showCategoryHandler = () => {
-        const updatedShowCategory = !showCategory;
-        setShowCategory(updatedShowCategory);
+    const controls = [];
+    for(let i in form){
+        controls.push(
+        <Control 
+        key={i} 
+        options={form[i].options} 
+        value={form[i].value} 
+        showOptions={form[i].showOptions}
+        name={i}
+        clicked={()=>toggleShowOptionsHandler(i)}
+        clickedOption={chooseOptionHandler}/>)
     };
 
-    const selectContainerClasses = showCategory ? 'select-container show' : 'select-container';
 
-    return(
-        <div className='shop-controls-container'>
-                <div className={selectContainerClasses}>
-                    <div className='select-label control-box'>Categories:</div>
-                    <div className='select-options-wrapp'>
-                        <div className='select-value-container control-box' onClick={showCategoryHandler}>
-                            <span>{categoriesOptions.find(el => el.value === category).displayValue}</span>
-                            <div className='select-svg-container'>
-                                <FontAwesomeIcon icon={faChevronDown}/>
-                            </div>
-                        </div>
-                        <div className='select-options-container'>
-                            <div className='option control-box' onClick={() => changeCategory('oil')}>
-                                <div className='select-svg-container'>
-                                    <FontAwesomeIcon icon={faPaintBrush}/>
-                                </div>
-                                <span>Oil paintings</span>
-                            </div>
-                            <div className='option control-box' onClick={() => changeCategory('acrilic')}>
-                                <div className='select-svg-container'>
-                                    <FontAwesomeIcon icon={faFlask}/>
-                                </div>
-                                <span value='acrilic'>Acrilic paintings</span>
-                            </div>
-                            <div className='option control-box' onClick={() => changeCategory('selectCat')}>
-                                <div className='select-svg-container'>
-                                    <FontAwesomeIcon icon={faWindowClose}/>
-                                </div>
-                                <span>Remove filter</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-    );
+
+
+    // const [category, setCategory] = useState('selectCat');
+    // const [showCategory, setShowCategory] = useState(false);
+
+
+    // const categoriesOptions = [{value:'selectCat', displayValue: 'Choose your type'},
+    //             {value:'nature', displayValue: 'Nature Morte'},
+    //             {value: 'bw', displayValue: 'Black and White'}];
+
+    // const changeCategory = (newCategoryValue) => {
+    //     setCategory(newCategoryValue);
+    //     setShowCategory(false);
+    // };
+
+    // const showCategoryHandler = () => {
+    //     const updatedShowCategory = !showCategory;
+    //     setShowCategory(updatedShowCategory);
+    // };
+
+    // const selectContainerClasses = showCategory ? 'select-container show' : 'select-container';
+
+    return controls;
+    // return(
+    //     <div className='shop-controls-container'>
+    //             <div className={selectContainerClasses}>
+    //                 <div className='select-label control-box'>Collections:</div>
+    //                 <div className='select-options-wrapp'>
+    //                     <div className='select-value-container control-box' onClick={showCategoryHandler}>
+    //                         <span>{categoriesOptions.find(el => el.value === category).displayValue}</span>
+    //                         <div className='select-svg-container'>
+    //                             <FontAwesomeIcon icon={faChevronDown}/>
+    //                         </div>
+    //                     </div>
+    //                     <div className='select-options-container'>
+    //                         <div className='option control-box' onClick={() => changeCategory('nature')}>
+    //                             <div className='select-svg-container'>
+    //                                 <FontAwesomeIcon icon={faPaintBrush}/>
+    //                             </div>
+    //                             <span>Nature Morte</span>
+    //                         </div>
+    //                         <div className='option control-box' onClick={() => changeCategory('bw')}>
+    //                             <div className='select-svg-container'>
+    //                                 <FontAwesomeIcon icon={faFlask}/>
+    //                             </div>
+    //                             <span value='acrilic'>Black and White</span>
+    //                         </div>
+    //                         <div className='option control-box' onClick={() => changeCategory('selectCat')}>
+    //                             <div className='select-svg-container'>
+    //                                 <FontAwesomeIcon icon={faWindowClose}/>
+    //                             </div>
+    //                             <span>Remove filter</span>
+    //                         </div>
+    //                     </div>
+    //                 </div>
+    //             </div>
+    //         </div>
+    // );
 };
