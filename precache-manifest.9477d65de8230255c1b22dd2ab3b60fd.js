acf160c5b2a326e5c5d18aed98c0c710e3742849
DenicaRuseva <drr_87@abv.bg>
2020-12-20 00:00:00 +0000

    Updates

---
+++ precache-manifest.9477d65de8230255c1b22dd2ab3b60fd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d87b5023fa3bccd1f656bc612d35ce",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "be6589d7269471075132",
+    "url": "/portfolio/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "48e84361ec2fc4002306",
+    "url": "/portfolio/static/css/main.9b447284.chunk.css"
+  },
+  {
+    "revision": "be6589d7269471075132",
+    "url": "/portfolio/static/js/2.dccea428.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/portfolio/static/js/2.dccea428.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48e84361ec2fc4002306",
+    "url": "/portfolio/static/js/main.132357b5.chunk.js"
+  },
+  {
+    "revision": "fae77958ff70cd04b7ea",
+    "url": "/portfolio/static/js/runtime-main.f40b4b3f.js"
+  },
+  {
+    "revision": "d55d8b685afa9e54759046998e59c101",
+    "url": "/portfolio/static/media/21752617_313159862486220_2846810308660514837_o.d55d8b68.jpg"
+  },
+  {
+    "revision": "22f37149779f883c98abf5f53c3bc77a",
+    "url": "/portfolio/static/media/dont-know-25547.22f37149.svg"
+  },
+  {
+    "revision": "5f8acf787006edef1c6c40342329a708",
+    "url": "/portfolio/static/media/map-of-the-world-2401458_1920.5f8acf78.jpg"
+  },
+  {
+    "revision": "bdd99ffbcffdbf4ff286fe5b09db3610",
+    "url": "/portfolio/static/media/map-of-the-world-2401458_640.bdd99ffb.jpg"
+  },
+  {
+    "revision": "17b0db2743211872fdacdf59dc80e60e",
+    "url": "/portfolio/static/media/robot.17b0db27.svg"
+  }
+]);
