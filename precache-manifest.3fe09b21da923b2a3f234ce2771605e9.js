388b7eae391481c7dfaa530c6e6c8dcc3985d6e2
DenicaRuseva <drr_87@abv.bg>
2021-02-12 23:22:05 +0000

    Updates

---
+++ precache-manifest.3fe09b21da923b2a3f234ce2771605e9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bcee3b59b4b5d76de6c632911268b11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18fda652b6ae75d2b7ab",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "ae2a2a6e098a54b0c95b",
+    "url": "/static/css/main.37408f22.chunk.css"
+  },
+  {
+    "revision": "18fda652b6ae75d2b7ab",
+    "url": "/static/js/2.947cb08a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.947cb08a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae2a2a6e098a54b0c95b",
+    "url": "/static/js/main.19c985b8.chunk.js"
+  },
+  {
+    "revision": "fa15da177897ef9cef8c",
+    "url": "/static/js/runtime-main.958a13e0.js"
+  },
+  {
+    "revision": "d55d8b685afa9e54759046998e59c101",
+    "url": "/static/media/21752617_313159862486220_2846810308660514837_o.d55d8b68.jpg"
+  },
+  {
+    "revision": "5f8acf787006edef1c6c40342329a708",
+    "url": "/static/media/map-of-the-world-2401458_1920.5f8acf78.jpg"
+  },
+  {
+    "revision": "bdd99ffbcffdbf4ff286fe5b09db3610",
+    "url": "/static/media/map-of-the-world-2401458_640.bdd99ffb.jpg"
+  },
+  {
+    "revision": "17b0db2743211872fdacdf59dc80e60e",
+    "url": "/static/media/robot.17b0db27.svg"
+  }
+]);
